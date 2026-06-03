2947b68b6eac527135ad8748a3e9b9a3a7571716
aablty <acrouple@yandex.ru>
2026-06-03 16:28:04 +0500

    users service

---
+++ server/src/users/users.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(email: string, password: string) {
+    return await this.prisma.user.create({
+      data: {
+        email,
+        password,
+      },
+    });
+  }
+
+  async findByEmail(email: string) {
+    return await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+  }
+}
